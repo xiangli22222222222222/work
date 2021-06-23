82f7e3a1788eeb2043bd5f4fb3f68853ed9a2f9a
admin <buaalx@qq.com>
2021-06-23 12:03:31 +0800

    211111

--- cv2/cv2-ceshi2.py
+++ cv2/cv2-ceshi2.py
@@ -1,49 +1,24 @@
-import cv2
-from datetime import datetime
-import time
+import socket
+def socket_service_data():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind(('127.0.0.1', 6666))  # 在同一台主机的ip下使用测试ip进行通信
+        # s.bind(('192.168.20.1', 6666))  #在不同主机或者同一主机的不同系统下使用实际ip
+        s.listen(10)
+    except socket.error as msg:
+        print(msg)
+        sys.exit(1)
 
-WIDTH = 1280
-HEIGHT = 720
-FPS = 10.0
-
-# 必须指定CAP_DSHOW(Direct Show)参数初始化摄像头,否则无法使用更高分辨率
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-# 设置摄像头设备分辨率
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
-# 设置摄像头设备帧率,如不指定,默认600
-cap.set(cv2.CAP_PROP_FPS, 15)
-# 建议使用XVID编码,图像质量和文件大小比较都兼顾的方案
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-n=0
-FILENAME = 'myvideo.avi'
-out = cv2.VideoWriter(FILENAME, fourcc, FPS, (WIDTH, HEIGHT))
-
-start_time = datetime.now()
-
-while n<=100:
-
-    ret, frame = cap.read()
+    print("Wait for Connection..................")
 
     while True:
-        if ret:
-            #cv2.imwrite('image2/' + str(n) + '.jpg', frame)
-            out.write(frame)
-            cv2.imshow('frame', frame)
-            if cv2.waitKey(1) == ord('q'):
-                break
-            录制5秒后停止
-            if (datetime.now()-start_time).seconds == 10:
-                cap.release()
-                print("录制结束")
-                break
-            监测到ESC按键也停止
-        else:
-            print("摄像头离线了")
-            break
-    n+=1
-    print(n)
-    time.sleep(100)
-out.release()
-cv2.destroyAllWindows()
-print("ok")
+        sock, addr = s.accept()
+        buf = sock.recv(1024)  #接收数据
+        buf = buf.decode()  #解码
+        print("The data from " + str(addr[0]) + " is " + str(buf))
+        print("Successfully")
+        # return buf
+        # sock.close()
+if __name__ == '__main__':
+    socket_service_data()
--- 往客户端传文件/Sever_1.py
+++ 往客户端传文件/Sever_1.py
@@ -6,39 +6,39 @@ server_1=socket.socket()
 server_1.bind(('127.0.0.1',8801))
 
 server_1.listen(3)
-number=0
-if number<10:
-    conn,addr=server_1.accept()
 
+
+
+while 1:
+    conn, addr = server_1.accept()
     file_size= conn.recv(1024)
+    if  file_size:
+        print("我看看第一次接受了啥:%s"%file_size)
 
-    file_size=file_size.decode('utf8')
+        file_size = file_size.decode('utf-8')
 
-    file_size=json.loads(file_size)
+        file_size = json.loads(file_size)
 
-    file_size1=file_size.get("data_size1")
+        file_size1=file_size.get("data_size1")
 
-    file_name=file_size.get("data_name1")
-
-
-    print(file_size1)
-
-
-    file_size1=int(file_size1)
-
-    print(file_size1)
-
-    f=open("e:/work/123/"+file_name,'wb')
-    recv_size = 0
-    while recv_size < file_size1:
-        data=conn.recv(1024)
-        print("这个长度有%s"%len(data))
-        recv_size += len(data)
-        f.write(data)
-        print("已经接受文件大小%s,总文件大小%s"%(recv_size,file_size1))
-    f.close()
-    number+=1
-    print(number)
-print("接受成功")
+        file_name=file_size.get("data_name1")
 
 
+        print(file_size1)
+
+
+        file_size1=int(file_size1)
+
+        print(file_size1)
+
+        f=open("e:/work/123/"+file_name,'wb')
+        recv_size = 0
+        while recv_size < file_size1:
+            data=conn.recv(1024)
+            print("这个长度有%s"%len(data))
+            recv_size += len(data)
+            f.write(data)
+            print("已经接受文件大小%s,总文件大小%s"%(recv_size,file_size1))
+        f.close()
+    # else:
+    #     print("接受完成")
